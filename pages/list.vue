6a3149875e5169d6e2c0b977a3934b5e730c6410
Kazuya <p7zh93d9@s.okayama-u.ac.jp>
2019-11-11 05:10:51 +0900

    add: tabでの切り替えデモ
    ジャンルごとに表示を分けるために，tabで分別．表示する表を変えることで，対応できると考えている．

---
+++ pages/list.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <b-tabs content-class="mt-3">
+            <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+            <b-tab title="list1"><list></list></b-tab>
+            <b-tab title="list2"><list></list></b-tab>
+            <b-tab title="list3"><list></list></b-tab>
+        </b-tabs>
+    </div>
+</template>
+
+<script>
+import list from './index.vue'
+
+export default {
+    components: {
+        list
+    },
+}
+</script>
+
+<style>
+
+</style>
