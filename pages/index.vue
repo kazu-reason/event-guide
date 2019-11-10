8f39e5c07aed522cd9a3b493ca04347057c2cbac
Kazuya <p7zh93d9@s.okayama-u.ac.jp>
2019-11-11 01:04:06 +0900

    add: main画面作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,29 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        event-guide
-      </h1>
-      <h2 class="subtitle">
-        My dandy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import util from '../assets/util'
 
 export default {
   components: {
-    Logo
+  },
+  data: function(){
+    return {
+      url: "http://localhost:3000/id2info.json"
+    }
+  },
+  mounted() {
+    util.axiosJson(this.url)
+    .then((val) => {
+      console.log(val)
+    })
+  },
+  methods: {
+
   }
 }
 </script>
